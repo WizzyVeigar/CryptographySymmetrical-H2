d6f0adef60efd5d1c95b46269122427e2bc08ee8
Kenneth Folke Larsen <5504355@RTS-365.dk>
2021-01-06 10:48:22 +0100

    Add project files.

---
+++ CryptographySymmetrical-H2/EncryptData.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+using System.Diagnostics;
+
+namespace CryptographySymmetrical_H2
+{
+    public enum Encrypters
+    {
+        DES,
+        TRIPLEDES,
+        AES
+    }
+
+    class EncryptData
+    {
+        public static Stopwatch watch;
+
+        public static void EncryptFolder(Encrypters encrypter)
+        {
+
+            byte[] key = null;
+            byte[] iv = null;
+
+            switch (encrypter)
+            {
+                case Encrypters.DES:
+                    key = GenerateNumbers(8);
+                    iv = GenerateNumbers(8);
+                    break;
+                case Encrypters.TRIPLEDES:
+                    key = GenerateNumbers(16);
+                    iv = GenerateNumbers(8);
+                    break;
+                case Encrypters.AES:
+                    key = GenerateNumbers(32);
+                    iv = GenerateNumbers(16);
+                    break;
+            }
+
+            EncryptFolder(encrypter, key, iv);
+        }
+
+        public static void EncryptFolder(Encrypters encrypter, byte[] key, byte[] iv)
+        {
+            watch = new Stopwatch();
+            watch.Start();
+            SymmetricAlgorithm algorithm = null;
+
+            switch (encrypter)
+            {
+                case Encrypters.DES:
+                    algorithm = DESCryptoServiceProvider.Create();
+                    break;
+                case Encrypters.TRIPLEDES:
+                    algorithm = TripleDESCryptoServiceProvider.Create();
+                    break;
+                case Encrypters.AES:
+                    algorithm = AesCryptoServiceProvider.Create();
+                    break;
+            }
+
+            algorithm.Mode = CipherMode.CBC;
+            algorithm.Padding = PaddingMode.PKCS7;
+
+            algorithm.Key = key;
+            algorithm.IV = iv;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                var cryptoStream = new CryptoStream(memoryStream, algorithm.CreateEncryptor(),
+                    CryptoStreamMode.Write);
+
+                foreach (string file in Directory.GetFiles(Environment.CurrentDirectory, "*.txt"))
+                {
+                    byte[] fileText = File.ReadAllBytes(file);
+
+                    cryptoStream.Write(fileText, 0, fileText.Length);
+                    cryptoStream.FlushFinalBlock();
+                    File.WriteAllBytes(file, memoryStream.ToArray());
+
+                }
+
+                watch.Stop();
+                Console.WriteLine("Stopwatch took: " + watch.ElapsedMilliseconds + " to complete");
+                Console.WriteLine("Key:" + algorithm.Key + "\n" +
+                    "IV: " + algorithm.IV);
+            }
+        }
+
+        /// <summary>
+        /// Method for decrypting data
+        /// </summary>
+        /// <param name="encrypter">What encryption is used</param>
+        /// <param name="message">The message to decrypt</param>
+        /// <param name="key">The encryption key</param>
+        /// <param name="iv">the encryption iv</param>
+        /// <returns>Returns a decrypted byte[]</returns>
+        public static void DecryptFolder(Encrypters encrypter, byte[] key, byte[] iv)
+        {
+            watch = new Stopwatch();
+            watch.Start();
+            SymmetricAlgorithm algorithm = null;
+
+            switch (encrypter)
+            {
+                case Encrypters.DES:
+                    algorithm = DESCryptoServiceProvider.Create();
+                    break;
+                case Encrypters.TRIPLEDES:
+                    algorithm = TripleDESCryptoServiceProvider.Create();
+                    break;
+                case Encrypters.AES:
+                    algorithm = AesCryptoServiceProvider.Create();
+                    break;
+            }
+
+            algorithm.Mode = CipherMode.CBC;
+            algorithm.Padding = PaddingMode.PKCS7;
+
+            algorithm.Key = key;
+            algorithm.IV = iv;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                var cryptoStream = new CryptoStream(memoryStream, algorithm.CreateDecryptor(),
+                    CryptoStreamMode.Write);
+
+                foreach (string file in Directory.GetFiles(Environment.CurrentDirectory, "*.txt"))
+                {
+                    byte[] fileText = File.ReadAllBytes(file);
+                    cryptoStream.Write(fileText, 0, fileText.Length);
+                    cryptoStream.FlushFinalBlock();
+                    File.WriteAllBytes(file, memoryStream.ToArray());
+
+                }
+
+                watch.Stop();
+                Console.WriteLine("Stopwatch took " + watch.ElapsedMilliseconds + " to decrypt");
+            }
+        }
+
+        public static byte[] GenerateNumbers(int length)
+        {
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] numbers = new byte[length];
+                rng.GetBytes(numbers);
+
+                return numbers;
+            }
+        }
+    }
+}
---
+++ CryptographySymmetrical-H2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptographySymmetrical_H2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //1.krypter eller ikke krypter
+                Console.WriteLine("Would you like to encrypt or decrypt?");
+                string encOrDec = Console.ReadLine().ToLower();
+
+                //2.Angiv nøgle, ikke angiv nøgle og iv?
+                string key = string.Empty;
+                string iv = string.Empty;
+                Console.WriteLine("Give key and iv yourself? y/n");
+                string keyIvInput = Console.ReadLine().ToLower(); ;
+                if (keyIvInput == "y")
+                {
+                    Console.WriteLine("Insert key");
+                    key = Console.ReadLine();
+                    Console.WriteLine("Insert iv");
+                    iv = Console.ReadLine();
+                }
+
+                //3.krypteringsform
+                for (int i = 0; i < Enum.GetNames(typeof(Encrypters)).Length; i++)
+                {
+                    Console.WriteLine(i + ". " + Enum.GetNames(typeof(Encrypters))[i]);
+                }
+                int encryptType = Convert.ToInt32(Console.ReadLine());
+
+                //5.Krypter og udskriv tid
+
+                if (encOrDec == "encrypt" || encOrDec == "1")
+                {
+                    if (keyIvInput == "y")
+                    {
+                        EncryptData.EncryptFolder((Encrypters)encryptType, Encoding.UTF8.GetBytes(key),Encoding.UTF8.GetBytes(iv));
+
+                    }
+                    else
+                    {
+                        EncryptData.EncryptFolder((Encrypters)encryptType);
+                    }
+                }
+                else if (encOrDec == "decrypt" || encOrDec == "2")
+                {
+                    if (keyIvInput == "y")
+                    {
+                        EncryptData.DecryptFolder((Encrypters)encryptType, Encoding.UTF8.GetBytes(key), Encoding.UTF8.GetBytes(iv));
+                    }
+                    else
+                    {
+                        Console.WriteLine("You cannot decrypt without giving a key and IV");
+                    }
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
